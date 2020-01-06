1a8b2f02c6bde723aa353b3f02074ea19d1319f8
chichiken <rnrnclzls1@gmail.com>
2020-01-06 23:50:34 +0900

    최종

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,15 @@
 //터치 슬라이더
-var mainSwiper = new Swiper ('#idx_content .idx_slide .swiper-container', {
+$(document).ready(function () {
+  var mainSwiper = new Swiper ('#idx_content .idx_slide .swiper-container', {
     loop: true,
     navigation: {
       nextEl: '.idx_slide .swiper-button-next',
       prevEl: '.idx_slide .swiper-button-prev'
+    },
+    pagination: {
+      el: '.consulting .swiper-pagination',
+      type: 'fraction',   //bullets(동그라미아이콘), fraction (현재/전체)
+      clickable: true,    //클릭하여 슬라이더 이동
     }
   });
+});
